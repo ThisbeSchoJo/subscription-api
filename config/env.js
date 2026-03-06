ed4dcb96481bf25b75efe1e98142b50740395d95
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2026-03-06 12:13:12 -0800

    Fixed bugs

--- config/env.js
+++ config/env.js
@@ -1,5 +1,12 @@
 import { config } from 'dotenv';
 
-config(options = { path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_ENV, ARCJET_KEY, QSTASH_URL, QSTASH_TOKEN, EMAIL_PASSORD } = process.env;
+export const {
+  PORT, NODE_ENV, SERVER_URL,
+  DB_URI,
+  JWT_SECRET, JWT_EXPIRES_IN,
+  ARCJET_ENV, ARCJET_KEY,
+  QSTASH_TOKEN, QSTASH_URL,
+  EMAIL_PASSWORD,
+} = process.env;
